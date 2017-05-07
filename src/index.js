c44c298c0b46bdca658c186391ba2a409a2d0ce9
Marijn Ophorst <marijn@monomelodies.nl>
2017-05-07 11:10:27 +0200

    this should all be moved here

--- src/index.js
+++ src/index.js
@@ -2,10 +2,26 @@
 "use strict";
 
 import 'angular-gettext';
-import Service from './Service';
+import Provider from './Provider';
+import Location from './Location';
 
 export default angular.module('monad.multilang', ['monad.cms', 'gettext'])
-    .service('monadLanguageService', Service)
+    .provider('monadLanguageService', Provider)
+    .service('monadLocation', Location)
+    .config(['$routeProvider', $routeProvider => {
+        $routeProvider.
+            when('/', {
+                template: '<noop></noop>',
+                controller: ['monadLocation', monadLocation => monadLocation.path('/')]
+            });
+    }])
+    .run(['$rootScope', 'monadLanguageService', ($rootScope, monadLanguageService) => {
+        $rootScope.$on('$routeChangeSuccess', (event, target) => {
+            if (target.params.language && target.params.language != monadLanguageService.current) {
+                monadLanguageService.current = target.params.language;
+            }
+        });
+    }])
     .name
     ;
 
