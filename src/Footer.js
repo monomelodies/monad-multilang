f754c238a30a1b3690352f3d24be816d8156b140
Marijn Ophorst <marijn@monomelodies.nl>
2017-05-11 09:08:40 +0200

    let this implement language selection

--- src/Footer.js
+++ src/Footer.js
@@ -3,6 +3,24 @@
 
 import FooterCtrl from 'monad-cms/components/Footer/Controller';
 
+let monadLanguageService = undefined;
+
 export default class Controller extends FooterCtrl {
+
+    constructor(_monadLanguageService_, ...args) {
+        super(...args);
+        monadLanguageService = _monadLanguageService_;
+    }
+
+    get language() {
+        return monadLanguageService.current || 'en';
+    }
+
+    get languages() {
+        return monadLanguageService.list || ['en'];
+    }
+
 };
+
+Controller.$inject = ['monadLanguageService'].concat(FooterCtrl.$inject);
 
